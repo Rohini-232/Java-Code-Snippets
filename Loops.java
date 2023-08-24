b34c0696254dc7d9a9d8a56c3f807659a56caabe
Rohini-232 <119690811+Rohini-232@users.noreply.github.com>
2023-08-24 14:47:23 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,32 @@
+class Loops
+{
+ public static void main(String[] args) 
+ { 
+ 
+ // For loop
+  for (int i = 1; i <5; i++) 
+  {
+   System.out.print(i+" ");   
+  }
+ // while loop   
+int x=1;
+  while(x<10)
+  {
+   System.out.println(x);
+   x++;
+  }
+ //do while loop 
+int a=1;
+  do
+  {
+   System.out.print(a +" ");
+   a++;
+  }
+  while(a<6); 
+
+  // foreach loop
+int ar[]={1,2,3,4,5};
+  for (int element:ar) 
+   System.out.print(element+" ");
+  }
+ }
---
+++ SwitchCase.java
@@ -0,0 +1,27 @@
+class SwitchCase
+{
+ public static void main(String[] args) 
+ {
+  int num=2;
+  switch(num)
+  {
+   case 1:
+    System.out.println("1st");
+    break;
+   case 2:
+    System.out.println("2nd");
+    break; 
+   case 3:
+    System.out.println("3rd");
+    break;
+   case 4:
+    System.out.println("4th");
+    break;
+   case 5:
+    System.out.println("5th");
+    break;
+   default:
+    System.out.println("No case matched");
+  }
+ }
+}
