59fcfd5d295a4e0b6c8d651bccd81bf3d55196d0
Rohini-232 <119690811+Rohini-232@users.noreply.github.com>
2023-08-24 13:04:23 +0530

    Add files via upload

---
+++ GretestOfTwoNumbers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class GretestOfTwoNumbers{
+    public static void main(String args[]){
+        int a=3, b=3;
+
+        if(a>b){
+            System.out.println("a is greater than b");
+        }
+        if(a==b){
+            System.out.println("a is equal to b");
+        }
+        else{
+            System.out.println("b is greater than a");
+        }
+
+
+    }
+}
