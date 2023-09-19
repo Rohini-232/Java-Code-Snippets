2dca557f161b3534b18a171b84f0de83f7153b36
Rohini-232 <119690811+Rohini-232@users.noreply.github.com>
2023-09-19 23:45:05 +0530

    Add files via upload

---
+++ Diamond_Pattern.java
@@ -0,0 +1,35 @@
+public class Diamond_Pattern{
+    public static void Pattern(int n){
+
+        for(int i=1; i<=n; i++){
+
+            //for space
+            for(int j=1; j<=(n-i); j++){
+                System.out.print(" ");
+            }
+
+            //for star
+            for(int j=1; j<=(2*i)-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n; i>=1; i--){
+
+            //for space
+            for(int j=1; j<=(n-i); j++){
+                System.out.print(" ");
+            }
+
+            //for star
+            for(int j=1; j<=(2*i)-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        Pattern(4);
+    }
+}
+     
