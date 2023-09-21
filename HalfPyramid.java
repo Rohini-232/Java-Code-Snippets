c22f361535a0749bfaa85d47e731054fbd9a9eb5
Rohini-232 <119690811+Rohini-232@users.noreply.github.com>
2023-09-21 21:25:00 +0530

    Add files via upload

---
+++ HalfPyramid.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class HalfPyramid{
+    public static void main(String args[]){
+        
+        int n=5;
+        for(int line=1; line<=n; line++){
+            for(int number=1; number<=(n-line+1); number++){
+                System.out.print(number);
+            }
+            System.out.println();
+        }
+    }
+}
