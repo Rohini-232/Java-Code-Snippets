3b4a42118590f312a865a4fe77eaff17d607a83c
Rohini-232 <119690811+Rohini-232@users.noreply.github.com>
2023-11-17 23:15:13 +0530

    Add files via upload

---
+++ MergeSort.java
@@ -0,0 +1,60 @@
+class MergeSort{
+
+    public static void print(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void ms(int arr[], int si, int ei){
+        if(si >= ei){
+            return;
+        }
+
+        int mid = (si + ei)/2;
+
+        ms(arr, si, mid);
+        ms(arr, mid+1, ei);
+        merge(arr, si, mid, ei);
+    }
+
+    public static void merge(int arr[], int si, int mid, int ei){
+        int i= si;
+        int j= mid+1;
+        int k= 0;
+
+        int temp[] = new int[ei-si+1];
+        while(i<=mid && j<=ei){
+            if(arr[i] < arr[j]){
+                temp[k] = arr[i];
+                i++;
+                
+            }else{
+                temp[k]= arr[j];
+                j++;
+                
+            }
+            k++;
+        }
+        while(i<=mid){
+            temp[k] = arr[i];
+            i++;
+            k++;
+        }
+        while(j<=ei){
+            temp[k]= arr[j];
+            j++;
+            k++;
+        }
+
+        for(k=0, i=si; i<temp.length; i++, k++){
+            arr[i] = temp[k];
+        }
+    }
+
+    public static void main(String[] args){
+        int arr[] = {6,3,9,5,2,8};
+        ms(arr, 0, arr.length-1);
+        print(arr);
+    }
+}
